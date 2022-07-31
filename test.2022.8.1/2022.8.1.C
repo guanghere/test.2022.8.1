fc009e1c28b591d0e1a854079abb97525a3d1189
HZKD513 <544228362@qq.com>
2022-07-31 22:06:09 +0800

    添加项目文件。

---
+++ test.2022.8.1/2022.8.1.C
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//int main(){
+//
+//	int learn = 0;
+//
+//	printf("做出选择\n");
+//
+//	printf("play(0) or learn(1) ?\n");
+//
+//	scanf("%d",&learn);
+//
+//	if (1==learn){
+//		printf("you will have a good job in the future !\n");
+//	}
+//
+//	else{
+//		printf("goddamn !\n");
+//	}
+//
+//	return 0;
+//}
+
+//
+//int main() {
+//	int count = 0;
+//	int a = 0;
+//	while (a <= 29999) {
+//		count++;
+//		a++;
+//		printf("敲代码ing ! %d\n",count);
+//	}
+//	if (a >= 30000)
+//	return	printf("你已经敲了%d行代码 ！\nyou will have a good job !\n", count);
+//}
+
+//int ADD(int x, int y) {
+//	return x + y;
+//}
+//
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int z =ADD(a, b);
+//	printf("%d", z);
+//	return 0;
+//}
